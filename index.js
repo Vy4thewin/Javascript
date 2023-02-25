8d80abfe6a4af521ff060ed4cca6e4ed6e874bfb
Vy <65354532+Vy4thewin@users.noreply.github.com>
2023-02-25 18:13:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ Give functionality to the buttons on the page
 
 // assigning elements by their ids in the html
 var btnverse=document.getElementById("btnRev");
-var word=document.getElementById("wrdverse").value;
 var fans=document.getElementById("rans");
 
 //hiding the label where the answer will appear
@@ -14,17 +13,16 @@ fans.style.display="none";
 
 //Looking for any click activity for the side bar's button
 btnverse.addEventListener("click",function(){
-
+    var word=document.getElementById("wrdverse").value;
     // Once button is click, enter the string into a for loop. Starting from the last character, append the char into the new string
     var reversestr='';
-    for(let l=word.length-1;l=>0;l--){
+    for(var l=word.length-1;l>=0;l--){
         reversestr+=word[l];
     }
 
     //assign the reverse string to be printed in the paragraph section and set visibility to true
     fans.style.display="inLine";
     fans.innerHTML=reversestr;
-
 
 }
 );
